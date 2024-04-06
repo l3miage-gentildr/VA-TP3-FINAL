e20c6c6a1813a7a9d586be3891d88395aec4f8fd
l3miage-gentildr <driss.gentil@etu.univ-grenoble-alpes.fr>
2024-04-06 23:59:45 +0200

    Test 2 is done

--- server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
@@ -2,6 +2,7 @@ package fr.uga.l3miage.spring.tp3.repositories;
 
 import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
 import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -34,7 +36,7 @@ public class CandidateRepositoryTest {
     }
 
     @Test
-    void findAllByTestCenterEntityCode() {
+    void testFindAllByTestCenterEntityCode() {
     // Set<CandidateEntity> findAllByTestCenterEntityCode(TestCenterCode code);
         TestCenterEntity testCenterEntityGRE = TestCenterEntity.builder()
                 .code(TestCenterCode.GRE)
@@ -66,4 +68,41 @@ public class CandidateRepositoryTest {
         assertThat(candidateEntities.stream().findFirst().get().getTestCenterEntity().getCode()).isEqualTo(TestCenterCode.GRE);
 
     }
+
+    @Test
+    void testFindAllByCandidateEvaluationGridEntitiesGradeLessThan() {
+    // Set<CandidateEntity> findAllByCandidateEvaluationGridEntitiesGradeLessThan(double grade);
+        CandidateEntity candidateEntity = CandidateEntity.builder()
+                .email("magnuscarlsen@jesuisunmail.com")
+                .birthDate(LocalDate.of(1990,11,30))
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity = CandidateEvaluationGridEntity.builder()
+                .grade(10)
+                .candidateEntity(candidateEntity)
+                .build();
+
+        candidateRepository.save(candidateEntity);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity);
+
+        CandidateEntity candidateEntity2 = CandidateEntity.builder()
+                .email("mvl@jesuisunmail.com")
+                .birthDate(LocalDate.of(1990,10,21))
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity2 = CandidateEvaluationGridEntity.builder()
+                .grade(4)
+                .candidateEntity(candidateEntity2)
+                .build();
+
+        candidateRepository.save(candidateEntity2);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity2);
+
+        Set<CandidateEntity> candidateEntities = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(10);
+
+        assertThat(candidateEntities).hasSize(1);
+        assertThat(candidateEntities.stream().findFirst().get().getBirthDate()).isEqualTo(LocalDate.of(1990,10,21));
+
+    }
+
 }
