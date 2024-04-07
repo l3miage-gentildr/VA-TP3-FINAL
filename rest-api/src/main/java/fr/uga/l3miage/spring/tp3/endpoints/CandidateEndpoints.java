f77fbb04dbc3ec241aea8678e8d7dcfa5037ca7e
l3miage-gentildr <driss.gentil@etu.univ-grenoble-alpes.fr>
2024-04-07 23:21:14 +0200

    Exo 3.2: Added GLaDOS

--- rest-api/src/main/java/fr/uga/l3miage/spring/tp3/endpoints/CandidateEndpoints.java
+++ rest-api/src/main/java/fr/uga/l3miage/spring/tp3/endpoints/CandidateEndpoints.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
+
 @Tag(name = "Gestion des candidat")
 @RestController
 @RequestMapping("/api/candidates")
@@ -23,4 +25,12 @@ public interface CandidateEndpoints {
     @GetMapping("/{candidateId}/average")
     Double getCandidateAverage(@PathVariable Long candidateId);
 
+    // Bonjour et bienvenue au centre d'enrichissement assisté par ordinateur, d'Aperture Science.
+    @Operation(description = "Ajoute une collection d'étudiants à un centre de test")
+    @ApiResponse(responseCode = "202", description = "Réussite de l'ajout des étudiants au centre de test")
+    @ApiResponse(responseCode = "404", description = "Le centre de test ou les étudiants n'existent pas", content = @Content)
+    @ApiResponse(responseCode = "400", description = "La requête est incorrecte", content = @Content)
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/{apertureScienceId}/addTestSubjects")
+    Boolean addCandidatesToTestCenter(@PathVariable Long apertureScienceId, @RequestBody Set<Long> testSubjectsIds);
 }
